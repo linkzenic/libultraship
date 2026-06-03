e5d4fb5596fa26386cfebdacea9ce134310dfcb5
Linkzenic <shanecus@gmail.com>
2026-06-03 12:32:19 -0700

    Restore framebuffer binding after MSAA resolve

--- src/fast/backends/gfx_opengl.cpp
+++ src/fast/backends/gfx_opengl.cpp
@@ -815,7 +815,7 @@ void GfxRenderingAPIOGL::ResolveMSAAColorBuffer(int fb_id_target, int fb_id_sour
 
     glBlitFramebuffer(0, 0, fb_src.width, fb_src.height, 0, 0, fb_dst.width, fb_dst.height, GL_COLOR_BUFFER_BIT,
                       GL_NEAREST);
-    glBindFramebuffer(GL_FRAMEBUFFER, mCurrentFrameBuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, mFrameBuffers[mCurrentFrameBuffer].fbo);
 
     glEnable(GL_SCISSOR_TEST);
 }
